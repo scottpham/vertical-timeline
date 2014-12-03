511ddfc4d14c94a6c4ff7f81ae209f92e4e1f872
scottpham <scottgpham@gmail.com>
2014-12-02 18:33:24 -0800

    click events awesome now

--- js/code.js
+++ js/code.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){
+	$("p").slideUp();
 	//if the h2 element is clicked
 	$("h2").click(function(){
 		console.log("Event fired");
-		$(this).next().slideToggle("slow");
+		
+		var graf = $(this).next();
+
+		$(this).toggleClass('no-after');
+
+		graf.slideToggle("slow");
+
+		$("p").not(graf).slideUp("slow");
+
 	});
 
 });
