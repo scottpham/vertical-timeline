1f3cb8c56582b396b3c9fb01880b84edf67b1a5b
scottpham <scottgpham@gmail.com>
2014-12-03 17:48:03 -0800

    change click events, add mobile behavior

--- js/code.js
+++ js/code.js
@@ -1,15 +1,19 @@
 $(document).ready(function(){
-	//$("p").not(".intro").slideUp();
-	//if the h2 element is clicked
+
+	//when h2 is clicked
 	$("h2").click(function(){
-		
+		//store var for sibline p tag
 		var graf = $(this).next();
-
-		$(this).toggleClass('no-after');
-
+		//slide in/out graf
 		graf.slideToggle("slow");
 
+		//change class, remove triangle pseudo-class
+		$(this).toggleClass('no-after');
+
+		//bring all other grafs up except sibline p tag and also intro p tag
 		$("p").not(graf).not(".intro").slideUp("slow");
+
+		$("h2").not(this).removeClass("no-after");
 
 	});
 
